efa571b25fca03d1d99e4d3720c4f77a039ac764
Travis Norris <travisynorris@gmail.com>
2019-05-11 14:17:01 -0400

    Working parse

--- WikipediaParser.py
+++ WikipediaParser.py
@@ -10,12 +10,13 @@ from bs4 import BeautifulSoup as soup
 startURL = "https://en.wikipedia.org/wiki/Donald_Trump"
 
 #filename to save results to
-filename = "WikipediaMap.csv"
+filename = "WikipediaMap1.csv"
 searched = []
 searched.append("/wiki/Donald_Trump")
 
 #number of levels to search
-levels = 1
+levels = 3
+linkMax = 20
 
 f = open(filename,"w")
 
@@ -26,7 +27,7 @@ def parsePage(myURL,count):
 		#open and read the wikipedia page
 		uClient = uReq(myURL)
 		page_html = uClient.read()
-		uClient.close();
+		uClient.close()
 
 		#find all paragraphs of text
 		page_soup = soup(page_html, "html.parser")
@@ -41,15 +42,20 @@ def parsePage(myURL,count):
 		print(str(count) + " , " + page_title)
 		f.write(page_title.replace(",","|")+ ",")
 
+		success = 0;
 
 		#search for every link on the page
 		for p in paragraphs:
+			if(success >= linkMax):
+				break
 			links = p.find_all("a")
 			for link in links:
+				if (success >= linkMax):
+					break
 				try:
 					url = link["href"]
 					#ignore sources and on-page links
-					if "#" not in url:
+					if "#" not in url and "File:" not in url and "http:" not in url and "%" not in url and "https:" not in url and "php?" not in url:
 						try:
 							title = link["title"].replace(",","|")
 							#ignore certain types of wikipedia pages
@@ -60,18 +66,18 @@ def parsePage(myURL,count):
 								if url not in searched and count > 0:
 									thisFrontier.append(url)
 									searched.append(url)
+									success += 1
 						except:
-							continue#print("ERROR: " + url)
-				except:
-					continue#print("ERRROR READING URL")
-		else:
-			#next line of the csv
-			f.write("\n");
-			if count == 0:
-				return
-			#search all the links on this page
-			for page in thisFrontier:
-				parsePage("https://en.wikipedia.org" + page, count - 1)
+							print("ERROR: " + url)
+				except Exception as e:
+					print("ERRROR READING URL")
+		#next line of the csv
+		f.write("\n");
+		if count == 0:
+			return
+		#search all the links on this page
+		for page in thisFrontier:
+			parsePage("https://en.wikipedia.org" + page, count - 1)
 
 	#not sure what these errors are but they're pretty infrequent
 	except Exception as e:
